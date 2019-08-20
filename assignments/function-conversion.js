0238f51895d7343cb1e666fb2cdfdade6d2ce097
Tora-Tora <53785365+Tora-Tora@users.noreply.github.com>
2019-08-19 23:32:27 -0500

    finish all the goal including stretch goal

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,10 +5,21 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => {
+  return param;
+};
+anotherFunction("Example");
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
@@ -20,11 +31,21 @@
 // };
 // subtract(1,2);
 
+let add = (param1, param2) => {
+  return param1 + param2;
+};
+add(1,2);
+
+let subtract = (param1, param2) => {
+  return param1 - param2;
+};
+subtract(1,2);
+
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
-// console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num) => {
+  return num * 3;
+});
+console.log(triple);
